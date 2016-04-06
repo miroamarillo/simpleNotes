52b6b6bb671d29272c7b893ef20da1e5656e5b75
Julian Pineda <miroamarillo@gmail.com>
2016-04-06 09:25:11 -0400

    cleaning indenting

--- gulpFile.js
+++ gulpFile.js
@@ -1,20 +1,20 @@
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
+var gulp 			= require('gulp'),
+	connect 		= require('gulp-connect'),
 
 gulp.task('connect', function() {
-  connect.server({
-    root: './',
-    livereload: true
-  });
+	connect.server({
+		root: './',
+		livereload: true
+	});
 });
 
 gulp.task('html', function () {
-  gulp.src('index.html')
-    .pipe(connect.reload());
+	gulp.src('index.html')
+		.pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['*.html','css/**', 'js/**'], ['html']);
+	gulp.watch(['*.html','css/**', 'js/**'], ['html']);
 });
 
 gulp.task('default', ['connect', 'watch']);
